55c39221d0ce3e1c1290e66a5deef28aa8bc92c5
tito guidotti <tito.guidotti@gmail.com>
2025-04-10 18:59:57 -0300

    ajuste inicial

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as Login20Auth } from "./Login20Auth.js";
+export { useAuth } from "./authContext.js";
+export { default as GoogleLogin } from "./GoogleLogin.js";
+export { default as MicrosoftLogin } from "./MicrosoftLogin.js";
