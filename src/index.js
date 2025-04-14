63aaebea22b393662713399236cbe6cda6617f65
tito guidotti <tito.guidotti@gmail.com>
2025-04-14 17:43:59 -0300

    Muda export de Pincode

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ export { default as Login20Auth } from "./Login20Auth.js";
 export { useAuth } from "./authContext.js";
 export { default as GoogleLogin } from "./GoogleLogin.js";
 export { default as MicrosoftLogin } from "./MicrosoftLogin.js";
-export {
-	PincodeLoginForm,
-	PincodeLoginEmailInput,
-	PincodeLoginPinInput,
-	PincodeLoginResendButton,
-	PincodeLoginClearButton,
+
+import PincodeLoginForm from "./PincodeLoginForm.js";
+import PincodeLoginEmailInput from "./PincodeLoginEmailInput.js";
+import PincodeLoginPinInput from "./PincodeLoginPinInput.js";
+import PincodeLoginResendButton from "./PincodeLoginResendButton.js";
+import PincodeLoginClearButton from "./PincodeLoginClearButton.js";
+import { usePincode } from "./PincodeLogin.js";
+
+export const Pincode = {
+	Form: PincodeLoginForm,
+	EmailInput: PincodeLoginEmailInput,
+	PinInput: PincodeLoginPinInput,
+	ResendButton: PincodeLoginResendButton,
+	ClearButton: PincodeLoginClearButton,
 	usePincode,
-} from "./PincodeLogin.js";
+};
